d71f16becf94165c04e9d27721e0600919c221f9
BigLev <40140251+B1gLev@users.noreply.github.com>
2024-03-05 12:04:30 +0100

    Bloodscreen

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
@@ -8,6 +9,10 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private GameObject bloodDrops;
     [SerializeField] private float maxHealth = 10f;
     [SerializeField] private Slider healthBar;
+    [SerializeField] private Image bloodScreenFX;
+    [SerializeField] private float fadeSmooth = 2f;
+    
+    private bool getDamage;
     private float currentHealth;
 
     private void Start()
@@ -15,11 +20,23 @@ public class PlayerHealth : MonoBehaviour
         currentHealth = maxHealth;
         healthBar.maxValue = maxHealth;
         healthBar.value = maxHealth;
+        getDamage = false;
+    }
+
+    private void Update()
+    {
+        if (getDamage) bloodScreenFX.color = new Color(255, 255, 255, .8f);
+        else
+        {
+            bloodScreenFX.color = Color.Lerp(bloodScreenFX.color, new(255, 255, 255, 0f), Time.deltaTime * fadeSmooth);
+        }
+        getDamage = false;
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
+        getDamage = true;
         healthBar.value = currentHealth;
         Instantiate(bloodDrops, transform.position, transform.rotation);
         if (currentHealth < 0) MakeDead();
@@ -28,5 +45,6 @@ public class PlayerHealth : MonoBehaviour
     private void MakeDead()
     {
         Destroy(gameObject);
+        bloodScreenFX.color = new(255,255, 255, 1f);
     }
 }
