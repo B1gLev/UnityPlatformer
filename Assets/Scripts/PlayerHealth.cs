613c160d1f403900c5381629555c2f6cddd171ea
BigLev <40140251+B1gLev@users.noreply.github.com>
2024-03-05 08:33:39 +0100

    Healthbar

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,24 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private GameObject bloodDrops;
     [SerializeField] private float maxHealth = 10f;
+    [SerializeField] private Slider healthBar;
     private float currentHealth;
-
-    private PlayerController playerController;
 
     private void Start()
     {
         currentHealth = maxHealth;
-        playerController = GetComponent<PlayerController>();
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
+        healthBar.value = currentHealth;
         Instantiate(bloodDrops, transform.position, transform.rotation);
         if (currentHealth < 0) MakeDead();
     }
